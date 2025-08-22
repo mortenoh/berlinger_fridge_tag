e652e7faf299b19a6d1db9ae62ae98a85340bdb1
Morten Hansen <morten@winterop.com>
2025-08-22 14:02:11 +0200

    chore: updated run api

--- run_api.py
+++ run_api.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     print("API will be available at: http://localhost:8000")
     print("Interactive docs at: http://localhost:8000/docs")
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
